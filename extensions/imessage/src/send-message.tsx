5cf5452633bebb5383003172468c262485c0706e
Darryl Brooks <darrylbrooks13@gmail.com>
2022-03-11 14:32:10 -0600

    chore: paint job of UI

--- extensions/imessage/src/send-message.tsx
+++ extensions/imessage/src/send-message.tsx
@@ -1,40 +1,98 @@
-import {List} from '@raycast/api';
-
+import { runAppleScript } from "run-applescript";
+import { Action, ActionPanel, Color, Form, FormTextArea, Icon, List, showToast, SubmitFormAction, ToastStyle } from "@raycast/api";
+import { useEffect, useState } from "react";
 export interface Contact {
-    id: string;
-    name: string;
-    avatar: string;
-    phoneNumber: string;
+  id: string;
+  name: string;
+  avatar: string;
+  phoneNumber: string;
 }
-const contacts: Contact[] = [
-    {
-        id: '1',
-        name: 'John Doe',
-        avatar: 'https://placeimg.com/64/64/any',
-        phoneNumber: '+1 (555) 555-5555',
-    },{
 
-        id: '2',
-        name: 'Mary Doe',
-        avatar: 'https://placeimg.com/64/64/any',
-        phoneNumber: '+1 (555) 555-5555',
-    },
-    {
-        id: '3',
-        name: 'Jane Doe',
-        avatar: 'https://placeimg.com/64/64/any',
-        phoneNumber: '+1 (555) 555-5555',
-    }
+const contacts = [
+  {
+    id: "1",
+    name: "John Doe",
+    avatar:
+      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
+    phoneNumber: "+1 (555) 555-5555",
+  },
+  {
+    id: "2",
+    name: "Darryl Brooks",
+    avatar:
+      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
+    phoneNumber: "+1 (555) 555-5555",
+  },
+  {
+    id: "3",
+    name: "Micheal Jackson",
+    avatar:
+      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
+    phoneNumber: "+1 (555) 555-5555",
+  },
+  {
+    id: "4",
+    name: "Kent Dodds",
+    avatar:
+      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
+    phoneNumber: "+1 (555) 555-5555",
+  },
 ];
+const getContacts = async () => {
+  const contacts = await runAppleScript(`
+    tell application "Contacts"
+    set contacts to {}
+      repeat with eachContact in (get every person)
+        set tempList to {}
+        set tempList to first name of eachContact & " " & last name of eachContact
+        set beginning of contacts to tempList
+      end repeat
+      return contacts
+    end tell
+    `);
 
+  return contacts;
+};
+
+const sendMessage = async () => {
+    setTimeout(()=>{
+        showToast({
+            title: "Message sent",
+            style: ToastStyle.Success,
+            message: "Message sent successfully",
+        })
+    },1000);
+}
+
+const TextBox = ({ contact }: { contact: Contact }): JSX.Element => {
+  return (
+    <Form onSubmit={sendMessage} actions={  
+    <ActionPanel>
+        <SubmitFormAction title="Send Message" onSubmit={sendMessage} />
+      </ActionPanel>}>
+      <FormTextArea id="text" title={`Message to ${contact.name}`} placeholder="Send message" />
+    </Form>
+  );
+};
 export default function SendMessage(): JSX.Element {
-
-    return(
-        <List searchBarPlaceholder='Search for contact'>
-            {contacts.map((contact, id) => (
-                <List.Item key={id} title={contact.name} icon={contact.avatar}/ >
-            ))}  
-        </List>
-        )
-
+  return (
+    <List searchBarPlaceholder="Search for contact">
+      {contacts.map((contact, id) => (
+        <List.Item
+          key={id}
+          title={contact.name}
+          icon={contact.avatar}
+          actions={
+            <ActionPanel>
+              <Action.Push
+                title="Message"
+                target={<TextBox contact={contact} />}
+                icon={{ source: Icon.List, tintColor: Color.PrimaryText }}
+              />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
 }
